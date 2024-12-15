c6c6c4f651a32e338b5804392c726da24f705126
Kilomiler <119633491+Kilomiler@users.noreply.github.com>
2024-12-15 11:58:22 -0500

    Layers + Markers Testing
    Added layers for inside and outside Dormont. 
    Partially filled out coords for inside house boxes (missing house box for Act 2 onward for House_Storage).
    Added main cast markers with image ref to wiki.gg.
    Added function where markers are semi-transparent depending on inside/outside layer selected. Note: Still need to work out the kinks for test popup content for markers.

--- newmain.js
+++ newmain.js
@@ -1,9 +1,18 @@
-var map = L.map('myMap', {crs: L.CRS.Simple,minZoom:2, maxZoom:6, maxNativeZoom:6, zoomDelta: 0.5,
-    zoomSnap: 0});
+var map = L.map('myMap', {
+    crs: L.CRS.Simple,
+    minZoom:1, 
+    maxZoom:6, 
+    maxNativeZoom:6, 
+    zoomDelta: 0.5,
+    zoomSnap: 0,
+    tileSize: L.point(160, 160)
+});
 
 var tile = {tileSize: L.point(160, 160)};
 
-L.tileLayer('https://raw.githubusercontent.com/in-stars-and-time-map/tiles/refs/heads/main/Act1/Dor_outside/{z}/{x}/{y}.png', tile).addTo(map)
+var dor_inside_map = L.tileLayer("https://raw.githubusercontent.com/in-stars-and-time-map/tiles/refs/heads/main/Act1/Dor_outside/{z}/{x}/{y}.png", tile)
+
+dor_inside_map.addTo(map)
 
 //set view to roughly coords for meadow, will change so it's focused on room box at a later date 
 map.setView([-138, 58], 6);
@@ -14,17 +23,9 @@ map.attributionControl.addAttribution(
     .setPrefix('Powered by <a href="https://leafletjs.com/">Leaflet</a>')
 
 //testing rectangle building outlines, let me play with my alphebet blocks, yo    
-/*var bounds = [[-122.8047, 44.1094], [-149.8086, 69.5703]];
-var bounds2 = [[-50.7602, 27.5007], [-122.8047, 92.0273]];
-var bounds3 = [[-46.8438, 92.0500], [-110.7813, 157.9219]];
-var bounds4 = [[-13.2344, 44.0313], [-50.7656, 69.5469]];
-var bounds5 = [[-35.7813, 2.0156], [-85.2969, 27.4990]];
-
-L.rectangle(bounds, {color: "#ff7800", weight: 0}).addTo(map);
-L.rectangle(bounds2, {color: "#ff4800", weight: 0}).addTo(map);
-L.rectangle(bounds3, {color: "#ff1800", weight: 0}).addTo(map);
-L.rectangle(bounds4, {color: "#ff1800", weight: 0}).addTo(map);    
-L.rectangle(bounds5, {color: "#ff1800", weight: 0}).addTo(map);*/
+/*
+Other
+*/
 
 var mapBoxes = {"type":"FeatureCollection","features":[
     {"type":"Feature","id":"01","properties":{"name":"MEADOW"},"geometry":{"type":"Polygon","coordinates":[[[44.1094, -122.8047], [69.5703, -122.8047], [69.5703, -149.8086], [44.1094, -149.8086], [44.1094, -122.8047]]]}},
@@ -35,12 +36,39 @@ var mapBoxes = {"type":"FeatureCollection","features":[
     //template, change with relevant latlngs: {"type":"Feature","id":"06","properties":{"name":"NameHere"},"geometry":{"type":"Polygon","coordinates":[[[1, 2],[1, 3],[4, 3],[4, 2],[1, 2]]]}},
 ]};
 
+var mapBoxes2 = {"type":"FeatureCollection","features":[
+    {"type":"Feature","id":"06","properties":{"name":"Shop"},"geometry":{"type":"Polygon","coordinates":[[[50.8281, -58.125], [70.9219, -58.125], [70.9219, -42.6406], [50.8281, -42.6406], [50.8281, -58.125]]]}},
+    {"type":"Feature","id":"07","properties":{"name":"Library"},"geometry":{"type":"Polygon","coordinates":[[[72.3125, -67.2188], [89.4219, -67.2188], [89.4219, -53.1719], [72.3125, -53.1719], [72.3125, -67.2188]]]}},
+    {"type":"Feature","id":"08","properties":{"name":"House_FrozenWife"},"geometry":{"type":"Polygon","coordinates":[[[70.1719, -102.6094], [84.0313, -102.6094], [84.0313, -87.1563], [70.1719, -87.1563], [70.1719, -102.6094]]]}},
+    {"type":"Feature","id":"09","properties":{"name":"House_Novels"},"geometry":{"type":"Polygon","coordinates":[[[43.6562, -103.125], [57.7656, -103.125], [57.7656, -88.9062], [43.6562, -88.9062], [43.6562, -103.125]]]}},
+    {"type":"Feature","id":"10","properties":{"name":"House_Change"},"geometry":{"type":"Polygon","coordinates":[[[28.0625, -92.9531], [42.2188, -92.9531], [42.2188, -78.7813], [28.0625, -78.7813], [28.0625, -92.9531]]]}},
+    {"type":"Feature","id":"11","properties":{"name":"House_Boulangerie"},"geometry":{"type":"Polygon","coordinates":[[[32.7813, -68.3125], [46.8594, -68.3125], [46.8594, -52.7344], [32.7813, -52.7344], [32.7813, -68.3125]]]}},
+    {"type":"Feature","id":"12","properties":{"name":"House_Flowers"},"geometry":{"type":"Polygon","coordinates":[[[93.7031, -54.75], [107.8125, -54.75], [107.8125, -40.5938], [93.7031, -40.5938], [93.7031, -54.75]]]}},
+    {"type":"Feature","id":"13","properties":{"name":"House_Cook"},"geometry":{"type":"Polygon","coordinates":[[[109.2031, -66.875], [126.1406, -66.875], [126.1406, -52.875], [109.2031, -52.875], [109.2031, -66.875]]]}}
+    //{"type":"Feature","id":"14","properties":{"name":"House_Storage"},"geometry":{"type":"Polygon","coordinates": }},
+]};
+
+var mira_pin = L.marker([-73.8125, 74.796875], {icon: L.icon({iconUrl: 'https://instarsandtime.wiki.gg/images/f/f5/GAMEICON_154.png', iconSize:[32, 32]})}).bindPopup('Mirabelle'),
+    isa_pin    = L.marker([-64.921875, 17.609375], {icon: L.icon({iconUrl: 'https://instarsandtime.wiki.gg/images/4/46/GAMEICON_152.png', iconSize:[32, 32]})}).bindPopup('Josy voice: Isabeauuuuu'),
+    bon_pin    = L.marker([-100.65625, 103.125], {icon: L.icon({iconUrl: 'https://instarsandtime.wiki.gg/images/5/5f/GAMEICON_150.png', iconSize:[32, 32]})}).bindPopup('Bonnie'),
+    odile_pin    = L.marker([-63.910697, 64.276706], {icon: L.icon({iconUrl: 'https://instarsandtime.wiki.gg/images/8/81/GAMEICON_153.png', iconSize:[32, 32]})}).bindPopup('[Odile, inside]').setOpacity(0.5)
+
+var main_chara = L.layerGroup([mira_pin, isa_pin, bon_pin, odile_pin]);
+
+var dor_in = L.tileLayer("https://raw.githubusercontent.com/in-stars-and-time-map/tiles/refs/heads/main/Act1/Dor_inside/{z}/{x}/{y}.png", {
+    tileSize: L.point(160, 160)
+}), onNewlocation;
+
+var dor_out = L.tileLayer("https://raw.githubusercontent.com/in-stars-and-time-map/tiles/refs/heads/main/Act1/Dor_outside/{z}/{x}/{y}.png", {
+    tileSize: L.point(160, 160)
+});
+
 //set style for mapBoxes, have all these properies aaaand
 function style() {
     return {
-        weight: 1,
+        weight: 2,
         color: 'black',
-        opacity: 0,
+        opacity: .5,
         fillColor: 'white',
         fillOpacity: 0
     };
@@ -59,12 +87,12 @@ function highlightFeature(e) {
 }
 
 function resetHighlight(e) {
-    geojson.resetStyle(e.target);
+    boxFunctions.resetStyle(e.target);
     info.update();
 }
 
 function zoomToRoom(e) {
-    map.fitBounds(e.target.getBounds());
+    map.fitBounds(e.target.getBounds().pad(0.1));
 }
 
 function onEachFeature(feature, layer) {
@@ -75,14 +103,14 @@ function onEachFeature(feature, layer) {
     });
 }
 
-geojson = L.geoJson(mapBoxes, {
+boxFunctions = L.geoJson(mapBoxes, {
     style: style,
     onEachFeature: onEachFeature
-}).addTo(map);
+});
 
 var info = L.control();
 
-info.onAdd = function (map) {
+info.onAdd = function () {
     this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
     this.update();
     return this._div;
@@ -100,14 +128,51 @@ info.addTo(map);
 //testing popup stuff and how it works
 var popup = L.popup()
     .setLatLng([-138, 58])
-    .setContent("I am a standalone popup.")
+    .setContent("Spawn")
     .openOn(map);
 
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
+        .setContent("Location: " + e.latlng.toString())
         .openOn(map);
 }
 
+
+var baseLayers = {
+    "Dormont (Outside)": dor_out,
+    "Dormont (Inside)": dor_in
+};
+
+var overlayMaps = {
+    "Main Characters": main_chara,
+    "Rooms": boxFunctions
+};
+
+var layerControl = L.control.layers(baseLayers, overlayMaps).addTo(map);
+
+function onOverlayAdd(e){
+    if (map.hasLayer(dor_in)) {
+        odile_pin
+            .setLatLng([-47.77, 66.276706])
+            .setOpacity(1.0)
+            .setPopupContent('Odile');
+        isa_pin.setOpacity(0.5); 
+        mira_pin.setOpacity(0.5); 
+        bon_pin.setOpacity(0.5);
+    } 
+    else if (map.hasLayer(dor_out)) {
+        odile_pin
+            .setLatLng([-63.910697, 64.276706])
+            .setOpacity(.5)
+            .setPopupContent('[Odile, inside]');
+        isa_pin.setOpacity(1.0); 
+        mira_pin.setOpacity(1.0); 
+        bon_pin.setOpacity(1.0);
+  }
+}
+
+
 map.on('click', onMapClick);
+
+map.on('baselayerchange', onOverlayAdd);
