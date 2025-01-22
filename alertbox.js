b3484e4de24de25d28d846830b39482df3634af6
Kilomiler <119633491+Kilomiler@users.noreply.github.com>
2025-01-22 17:15:26 -0500

    Add files via upload

--- alertbox.js
+++ alertbox.js
@@ -5,12 +5,10 @@ myDialog.showModal();
 document.getElementById('close-popup').addEventListener('click', () => {
 if (document.getElementById('do-not-show-again').checked) {
     localStorage.setItem('popupShown', 'true');
-    alert('passed do-not-show check');
 }
 myDialog.remove();
 });
 // Check if the popup should be shown
 if (localStorage.getItem('popupShown')) {
     myDialog.close();
-    alert('Popup closed');
 }
